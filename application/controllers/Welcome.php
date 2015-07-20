93cf42e6ed833837eb827c2fce6b8a74ce15714a
Sergey <s.finciuc@voodoo-studio.ch>
2015-07-20 10:50:49 +0300

    First commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+    public $data;
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
